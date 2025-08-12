56023123a2d53e0af2c257a2836656b372670e57
Rodrigo Capre <rodrigocapre@gmail.com>
2025-08-12 18:08:44 -0300

    Crea endpoints para CRUD con django REST framework

--- backend/coffees/admin.py
+++ backend/coffees/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Coffee
 
+admin.site.register(Coffee)
 # Register your models here.
--- backend/coffees/views.py
+++ backend/coffees/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Coffee
+from .serializers import CoffeeSerializer
 
 # Create your views here.
+class CoffeeViewSet(viewsets.ModelViewSet):
+    queryset = Coffee.objects.all()
+    serializer_class = CoffeeSerializer
