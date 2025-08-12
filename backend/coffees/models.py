5c1ebb6541149900d9ce9802d8ce1bfacc77843e
Rodrigo Capre <rodrigocapre@gmail.com>
2025-08-12 16:53:51 -0300

    Agrega modelo Coffee

---
+++ backend/coffees/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Coffee(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    origin = models.CharField(max_length=100)
+    roast_level = models.CharField(max_length=50)
+    flavor_notes = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- backend/coffemgmtsys/settings.py
+++ backend/coffemgmtsys/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'coffes',
+    'coffees',
 ]
 
 MIDDLEWARE = [
