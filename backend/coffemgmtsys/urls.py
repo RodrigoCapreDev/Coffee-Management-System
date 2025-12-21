3c2b7e283e15824c2491f588f7e8cee8594a393f
Rodrigo Capre <rodrigocapre@gmail.com>
2025-12-21 12:21:07 -0300

    feat: versionar api a v1 y añadir endpoints de documentación swagger

--- backend/coffemgmtsys/urls.py
+++ backend/coffemgmtsys/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from coffees.views import CoffeeViewSet
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router=routers.DefaultRouter()
 router.register(r'coffees', CoffeeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/v1/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
