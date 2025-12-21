4f89f864113c9816d1702494d6bcaebc16f4b029
Rodrigo Capre <rodrigocapre@gmail.com>
2025-12-21 12:52:10 -0300

    feat: aumentar la capacidad de dígitos en el campo de precio

--- backend/coffees/models.py
+++ backend/coffees/models.py
@@ -6,7 +6,7 @@ class Coffee(models.Model):
     origin = models.CharField(max_length=100)
     roast_level = models.CharField(max_length=50)
     flavor_notes = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
