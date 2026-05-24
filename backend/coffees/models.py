bf7fae1fef8eb8b71e08c7a0350259cf7c70f35a
Rodrigo Capre <rodrigocapre@gmail.com>
2026-05-24 18:59:33 -0300

    feat: add stock, wiegh_grams and is_available

--- backend/coffees/models.py
+++ backend/coffees/models.py
@@ -32,11 +32,19 @@ class Coffee(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     flavor_notes = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     origin = models.ForeignKey(
         Origin, on_delete=models.SET_NULL, related_name="coffees", null=True
     )
+
+    #--- comercial ---
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight_grams= models.PositiveIntegerField(
+        default=250, help_text="Peso del paquete en gramos"
+    )
+    stock = models.PositiveIntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+    
 
     def __str__(self):
         return self.name
